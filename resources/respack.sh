58839dcd5da9e9273a0e5292c76db1c71d536aab
Emil Muratov <gpm@hotplug.ru>
2020-11-29 01:09:55 +0300

    WEB: dark/light themes
     - ported themes from FireLAMP project, tnx to @kostyamat, @pavelts, @kdn and others
     - removed ace editor files
    
    Signed-off-by: Emil Muratov <gpm@hotplug.ru>

--- resources/respack.sh
+++ resources/respack.sh
@@ -2,25 +2,19 @@
 
 echo "Preparing resources for EmbUI FS image" 
 
-mkdir -p ./data/css ./data/js ./data/login ./data/extras
-cat html/css/*.css | gzip -9 > ./data/css/all.css.gz
+mkdir -p ./data/css ./data/js
+cat html/css/pure-min.css html/css/grids.css | gzip -9 > ./data/css/pure.css.gz
+cat html/css/*_default.css | gzip -9 > ./data/css/style.css.gz
+cat html/css/*_light.css | gzip -9 > ./data/css/style_light.css.gz
+cat html/css/*_dark.css | gzip -9 > ./data/css/style_dark.css.gz
+
 cp html/css/*.jpg ./data/css/
+cp html/css/*.webp ./data/css/
+
 cat html/js/*.js | gzip -9 > ./data/js/all.js.gz
 cat html/index.html | gzip -9 > ./data/index.html.gz
 cat html/favicon.ico | gzip -9 > ./data/favicon.ico.gz
-cp html/.exclude.files > ./data/
-
-cat html/login/index.htm | gzip -9 > ./data/login/index.htm.gz
-cat html/login/favicon.ico | gzip -9 > ./data/login/favicon.ico.gz
-
-cat html/extras/edit.htm | gzip -9 > ./data/extras/edit.htm.gz
-cat html/extras/ace.ico | gzip -9 > ./data/extras/ace.ico.gz
-cat html/extras/acefull.js | gzip -9 > ./data/extras/acefull.js.gz
-cat html/extras/mode-json.js | gzip -9 > ./data/extras/mode-json.js.gz
-cat html/extras/worker-css.js | gzip -9 > ./data/extras/worker-css.js.gz
-cat html/extras/worker-html.js | gzip -9 > ./data/extras/worker-html.js.gz
-cat html/extras/worker-javascript.js | gzip -9 > ./data/extras/worker-javascript.js.gz
-cat html/extras/worker-json.js | gzip -9 > ./data/extras/worker-json.js.gz
+cp html/.exclude.files ./data/
 
 cd ./data
 zip --filesync -r -0 --move --quiet ../data.zip ./*
