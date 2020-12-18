5494935f9ff73d5b440bc19d296400d577d4179d
Emil Muratov <gpm@hotplug.ru>
2020-12-19 02:28:39 +0300

    Checkbox now returns "1"/"0" as value instead of "true"/"false"
    Signed-off-by: Emil Muratov <gpm@hotplug.ru>

--- examples/ex_generic/src/interface.h
+++ examples/ex_generic/src/interface.h
@@ -3,6 +3,7 @@
 void block_menu(Interface *interf, JsonObject *data);
 void block_demopage(Interface *interf, JsonObject *data);
 void action_demopage(Interface *interf, JsonObject *data);
+void action_blink(Interface *interf, JsonObject *data);
 void pubCallback(Interface *interf);
 
 //void remote_action(RA action, ...);
