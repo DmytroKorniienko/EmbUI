5b718f80b0881b4f1fe999e62597cb2884e87f18
Dmytro Korniienko <Dmytro.Korniienko@volo.global>
2021-07-27 17:30:01 +0300

    library update

--- examples/02_sensors/src/src.ino
+++ examples/02_sensors/src/src.ino
@@ -2,4 +2,6 @@
  * Для работы WebUI необходимо залить в контроллер образ фаловой системы LittleFS с web-ресурсами.
  * Подготовленные ресурсы для создания образа можно развернуть из архива: https://github.com/DmytroKorniienko/EmbUI/raw/main/resources/data.zip .
  * В ArduinoIDE каталог data должна распологаться в том же каталоге, что этот файл; в Platformio https://platformio.org/ это, обычно, каталог data в корне проекта.
+ *
+ * Attention: Please check dependencies in a description before using, Arduino IDE can't download some of them (Внимание, проверьте зависимости перед использованием, среда не может скачать некоторые из них): https://github.com/DmytroKorniienko/EmbUI/
  */
