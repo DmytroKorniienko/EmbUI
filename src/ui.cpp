58df35b18b8fcc260ef39377321037630e31596b
Dmytro Korniienko <Dmytro.Korniienko@volo.global>
2021-08-26 21:44:05 +0300

    fix2

--- src/ui.cpp
+++ src/ui.cpp
@@ -99,9 +99,10 @@ void Interface::select(const String &id, const String &value, const String &labe
     
     section_stack.end()->idx--;
     uint8_t idx = section_stack.end()->idx;
-    do { // после того как было дробление фрейма, индекс может не соответствовать!!!
+    // после того как было дробление фрейма, индекс может не соответствовать!!!
+    while (!section_stack.end()->block.getElement(idx) && idx){
         idx--;
-    } while (!section_stack.end()->block.getElement(idx) && idx);
+    }
 
     json_section_begin(FPSTR(P_options), "", false, false, false, section_stack.end()->block.getElement(idx));
 }
