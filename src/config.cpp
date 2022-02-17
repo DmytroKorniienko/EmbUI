1ac4aee46ff1cdeb1fe7f258fd740e92c8e1dd50
Dmytro Korniienko <Dmytro.Korniienko@volo.global>
2022-02-17 18:44:24 +0200

    config autosave first run fix

--- src/config.cpp
+++ src/config.cpp
@@ -17,8 +17,9 @@ void EmbUI::save(const char *_cfg, bool force){
     File configFile;
     if (_cfg == nullptr) {
         LOG(println, F("UI: Save default main config file"));
-        if(millis()>(sysData.asave * EMBUI_AUTOSAVE_MULTIPLIER * TASK_SECOND)+1000)
-            LittleFS.rename(FPSTR(P_cfgfile),FPSTR(P_cfgfile_bkp)); // unstable currently... sad, skip first rename prob
+        //LOG(printf_P, PSTR("%d, %d\n"),millis(),(sysData.asave * EMBUI_AUTOSAVE_MULTIPLIER * TASK_SECOND)+5000);
+        if(millis()>(sysData.asave * EMBUI_AUTOSAVE_MULTIPLIER * TASK_SECOND)+5000)
+            LittleFS.rename(FPSTR(P_cfgfile),FPSTR(P_cfgfile_bkp)); // unstable currently... sad, skipping first rename try
         configFile = LittleFS.open(FPSTR(P_cfgfile), "w"); // PSTR("w") использовать нельзя, будет исключение!
     } else {
         LOG(printf_P, PSTR("UI: Save %s main config file\n"), _cfg);
